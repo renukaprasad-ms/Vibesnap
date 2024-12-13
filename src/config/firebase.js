bb2fd1a8e3e9801223a6c676213d7e1f2d070da1
renukaprasadms <renukaprasadms00@gmail.com>
2024-12-13 16:52:42 +0530

    added build

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,20 +1,18 @@
+
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-
-// Use import.meta.env for Vite environment variables
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyD8W5xZixWB6tZsHYd4vATrYX-jq42By_k",
+  authDomain: "vibesnap-ecf11.firebaseapp.com",
+  projectId: "vibesnap-ecf11",
+  storageBucket: "vibesnap-ecf11.firebasestorage.app",
+  messagingSenderId: "658859114321",
+  appId: "1:658859114321:web:48e442c6d97c8ed1c3dd70",
+  measurementId: "G-GFT1GNN3BK"
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
-export const db = getFirestore(app);
+export const db = getFirestore(app)
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  },
+  define: {
+    'process.env': {}
+  }
 })
